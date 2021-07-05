dacbb2e763f90bdfc9ffb08ec5137c20d2222e9d
DmitrovicAleksa <aleksa.dmitrovic@vivifyideas.com>
2021-07-05 13:30:12 +0200

    user auth is done

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.hashers import make_password
 
 
 class UserManager(BaseUserManager):
--- users/serializers.py
+++ users/serializers.py
@@ -7,16 +7,6 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     def validate_password(self,value):
         return make_password(value)
-    
-    def create(self, validated_data):
-
-        user = User.objects.create_user(
-            email=validated_data['email'],
-            first_name = validated_data['first_name'],
-            last_name = validated_data['last_name'],
-            password=validated_data['password'],
-        )
-        return user
 
     class Meta:
         model = User
